3c6060126f60ea4104dd1d67ca3b13148804e708
Dominik Stańczak <stanczakdominik@gmail.com>
2016-02-29 10:25:17 +0100

    dioda led zapalona

--- lab0/main.c
+++ lab0/main.c
@@ -10,5 +10,12 @@
 
 int main(void)
 {
+	//czy pin z danego portu jest wejście\wyjście?
+	DDRA = 0xFF; //data direction register A - 4 jedynki, 4 jedynki
+	// ^ pójście na skróty, ustawiamy wszystkie na wyjście
+	// 0b11111111 = 0xFF = 255
 
+	PORTA = 0x00; //diody mają z drugiej strony high, trzeba ustawić low by była różnica napięć
+
+	while(1){} //czekaj po zakończeniu programu
 }
