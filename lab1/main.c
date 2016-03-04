47f0beee7a10a2a54a6d2b5d8df823bf0d4483f2
Dominik Stańczak <stanczakdominik@gmail.com>
2016-03-04 17:49:54 +0100

    silnik krokowy, zawraca, stopuje, przyspiesza

--- lab1/main.c
+++ lab1/main.c
@@ -5,19 +5,50 @@
  *      Author: psm11
  */
 
-
+#include <stdint.h>
 #include <avr/io.h>
 #include "longdelay.h"
 
 int main(void)
 {
 	DDRC = 0xFF;
+	DDRD = 0x00;
+	PORTD = 0xFF;
 
+	uint8_t status = 0b0011;
+	uint8_t state = 0;
+	uint16_t speed = 10;
+	uint8_t direction = 1;
 	while(1)
 	{
-		longdelay(1000);
-		PORTC = 0b01101000;
-		longdelay(1000);
-		PORTC = ~0b01101000;
+		if (!(PIND & (1<<PD0)))
+		{
+			longdelay(100);
+			if (!(PIND & (1<<PD0)))
+				state = (state + 1)%2;
+		}
+		if (!(PIND & (1<<PD1)))
+		{
+			speed = (speed + 2)%50;
+		}
+		if (!(PIND & (1<<PD2)))
+		{
+			longdelay(100);
+			if (!(PIND & (1<<PD2)))
+				direction = (direction + 1)%2;
+		}
+		if(state)
+		{
+			if(direction)
+			{
+				status = (status << 1) | (status >> 3);
+			}
+			else
+			{
+				status = (status >> 1) | (status << 3);
+			}
+			PORTC = status;
+			longdelay(speed);
+		}
 	}
 }
